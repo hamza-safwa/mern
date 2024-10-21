a59ab25b0287feffaa25dde5b425b12bf4751346
hamza-safwa <119676651+hamza-safwa@users.noreply.github.com>
2024-10-21 21:22:11 +0500

    modified app navigation along ('/')

--- rest.js
+++ rest.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.route('/')
+
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.get('/', (req, res) => {
